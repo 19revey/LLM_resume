c02c20dce6836c84695d4def433fab36bd74567f
yduan92 <yduan92@gatech.edu>
2024-01-22 22:15:48 +0000

    merge

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-from config import GOOGLE_API_KEY
+# from config import GOOGLE_API_KEY
 
-genai.configure(api_key=GOOGLE_API_KEY)
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input,pdf_cotent,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
