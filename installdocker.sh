0d7abd7de72253b5ceb9bdd93e104c74203d042c
yduan92 <yduan92@gatech.edu>
2024-01-23 01:21:55 +0000

    install docker on ecs

---
+++ installdocker.sh
@@ -0,0 +1,15 @@
+#optinal
+
+sudo apt-get update -y
+
+sudo apt-get upgrade
+
+#required
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker ubuntu
+
+newgrp docker
