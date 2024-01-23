39f2dc647216b9d71c71354d31290ecdd916b9fb
yduan92 <yduan92@gatech.edu>
2024-01-23 13:57:34 -0800

    specify googlegenerativeai version

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key='AIzaSyAkOcFbE01fv_5nEU_YDjReVz8TfMXHN2w')
-
-model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content('Say hi')
-
-print(response.text)
