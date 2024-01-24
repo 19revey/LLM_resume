13a6409006e72c60a5afaee4eccaa39a08bc513e
yduan92 <yduan92@gatech.edu>
2024-01-23 19:48:54 -0800

    set temperature, reduce randomness

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input,pdf_content,prompt):
     model=genai.GenerativeModel('gemini-pro')
-    response=model.generate_content([input,pdf_content,prompt])
+    generation_config = genai.GenerationConfig(
+    temperature=0.0
+    )
+    response=model.generate_content([input,pdf_content,prompt],generation_config=generation_config)
     return response.text
 
 def input_pdf_text(uploaded_file):
